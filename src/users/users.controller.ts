9df101ea5772321830e93cb6342e1b65ad6a875d
Awoniran <awoniranopeyemi@gmail.com>
2022-08-01 17:33:33 +0100

    feat

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dtos';
 
-@Controller('reports')
-export class UserController {}
+import { Post, Get, Patch, Param } from '@nestjs/common';
+import { createMessageDto } from 'messages/src/messages/dtos/create-message.dtos';
+
+@Controller('auth')
+export class UserController {
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto) {}
+}
