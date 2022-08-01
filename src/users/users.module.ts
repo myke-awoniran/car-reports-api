76d02276b3f6356dc556d34ce967a3d551a9f235
Awoniran <awoniranopeyemi@gmail.com>
2022-08-01 11:52:00 +0100

    feat

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
+import { UserController } from './users.controller';
 
 @Module({
-  providers: [UsersService]
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UserController],
+  providers: [UsersService],
 })
 export class UsersModule {}
